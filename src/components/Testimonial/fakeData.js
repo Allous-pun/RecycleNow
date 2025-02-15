b0aa30a1a38ab7f73273c692d279b1fa078130a7
SPANNERBOY <sulee9610@gmail.com>
2025-02-15 20:54:17 +0300

    commit message

--- src/components/Testimonial/fakeData.js
+++ src/components/Testimonial/fakeData.js
@@ -4,14 +4,14 @@ export const fakeData = [
         img: 'https://via.placeholder.com/150', 
         name: 'Ian Dan',
         profession: 'Founder & CEO, RecycleNow',
-        message: "Our vision at RecycleNow is to revolutionize waste management through education and innovation. We're dedicated to creating a sustainable future for our communities."
+        message: "Our vision at RecycleNow is to revolutionize waste management through education and innovation. We're dedicated to creating a sustainable future for our communities.",
     },
     {
         id: 2,
         img: 'https://via.placeholder.com/150', 
         name: 'Nelson Tom',
         profession: 'Financial Expert',
-        message: "Investing in RecycleNow is not just a financial decision, but a commitment to a greener future. Their innovative approach is setting new industry standards."
+        message: "Investing in RecycleNow is not just a financial decision, but a commitment to a greener future. Their innovative approach is setting new industry standards.",
     },
     {
         id: 3,
@@ -19,21 +19,21 @@ export const fakeData = [
 
 
         profession: 'Tenant',
-        message: "Thanks to RecycleNow, our estates are cleaner, and we've become more conscious of our waste disposal habits. It's a win-win for the environment and our community."
+        message: "Thanks to RecycleNow, our estates are cleaner, and we've become more conscious of our waste disposal habits. It's a win-win for the environment and our community.",
     },
     {
         id: 4,
         img: 'https://via.placeholder.com/150', 
         name: 'Sir  K',
         profession: 'Nema Official',
-        message: "Partnering with RecycleNow has amplified our impact in promoting environmental sustainability. Their expertise and dedication are truly commendable."
+        message: "Partnering with RecycleNow has amplified our impact in promoting environmental sustainability. Their expertise and dedication are truly commendable.",
     },
     {
         id: 5,
         img: 'https://via.placeholder.com/150',
         name: 'Tony Wekesa',
         profession: 'Businessman',
-        message:" RecycleNow's platform has expanded our reach and boosted sales of eco-friendly products. We're proud to be part of a movement towards a sustainable future."
+        message:" RecycleNow's platform has expanded our reach and boosted sales of eco-friendly products. We're proud to be part of a movement towards a sustainable future.",
 
 
     },
@@ -42,6 +42,6 @@ export const fakeData = [
         img: 'https://via.placeholder.com/150',
         name: 'Musa Mona',
         profession: 'Tech Entrepreneur',
-        message: "RecycleNow has been a key partner in our journey to digital transformation. Their innovative solutions and commitment to excellence are unmatched."
+        message: "RecycleNow has been a key partner in our journey to digital transformation. Their innovative solutions and commitment to excellence are unmatched.",
     },
 ];
