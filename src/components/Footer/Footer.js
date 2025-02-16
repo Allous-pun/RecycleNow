373a317f57c3e93023a2cde822584c92531fa8ef
pundit <pundit@pundit.pundit.com>
2025-02-16 21:43:38 +0300

    latest updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,39 +18,34 @@ const Footer = () => {
     const [openService, setOpenService] = useState(null);
 
     const toggleService = (index) => {
-        if (openService === index) {
-            setOpenService(null);
-        } else {
-            setOpenService(index);
-        }
+        setOpenService(openService === index ? null : index);
     };
 
     return (
         <>
-          {/* Newsletter Section */}
-<div className={styles.newsletterContainer}>
-    <div className={styles.newsletterContent}>
-        <p className={styles.newsletterText}>
-            Subscribe to our newsletter for <br />
-            updates on our programs and initiatives.
-        </p>
-        <div className={styles.newsletterInputContainer}>
-            <input
-                type="email"
-                placeholder="Enter your email"
-                required
-                className={styles.newsletterInput}
-            />
-            <button
-                className={styles.subscribeButton}
-                onClick={() => (window.location.href = "mailto:info@recyclenow.com")}
-            >
-                Subscribe
-            </button>
-        </div>
-    </div>
-</div>
-
+            {/* Newsletter Section */}
+            <div className={styles.newsletterContainer}>
+                <div className={styles.newsletterContent}>
+                    <p className={styles.newsletterText}>
+                        Subscribe to our newsletter for <br />
+                        updates on our programs and initiatives.
+                    </p>
+                    <div className={styles.newsletterInputContainer}>
+                        <input
+                            type="email"
+                            placeholder="Enter your email"
+                            required
+                            className={styles.newsletterInput}
+                        />
+                        <button
+                            className={styles.subscribeButton}
+                            onClick={() => (window.location.href = "mailto:info@recyclenow.com")}
+                        >
+                            Subscribe
+                        </button>
+                    </div>
+                </div>
+            </div>
 
             {/* Footer Section */}
             <div style={{ backgroundColor: '#266F46', paddingTop: '89px', paddingBottom: '90px' }}>
@@ -58,7 +53,7 @@ const Footer = () => {
                     <div className="row">
                         {/* Column 1 */}
                         <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
-                            <p className={styles.logo}>RecycleNow is located along Kisumu - Kakamega Highway in  Lake Basin Mall</p>
+                            <p className={styles.logo}>RecycleNow is located along Kisumu - Kakamega Highway in Lake Basin Mall</p>
                         </div>
 
                         {/* Column 2 */}
@@ -71,11 +66,7 @@ const Footer = () => {
                                         <FiChevronDown style={{ marginLeft: '8px', cursor: 'pointer' }} />
                                     </div>
                                     {openService === index && (
-                                        <div
-                                            className={styles.text}
-                                            style={{ cursor: 'pointer' }}
-                                        >
-                                            {/* Link to the PDF */}
+                                        <div className={styles.text} style={{ cursor: 'pointer' }}>
                                             <a href={service.pdfUrl} target="_blank" rel="noopener noreferrer">
                                                 View {service.name}
                                             </a>
@@ -88,9 +79,9 @@ const Footer = () => {
                         {/* Column 3 */}
                         <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
                             <p className={styles.head}>Our Services</p>
-                            <p className={styles.text}>Mass Education </p>
+                            <p className={styles.text}>Mass Education</p>
                             <p className={styles.text}>Waste Segregation</p>
-                            <p className={styles.text}>Waste collection and Disposal</p>
+                            <p className={styles.text}>Waste Collection and Disposal</p>
                             <p className={styles.text}>Waste Recycling</p>
                         </div>
 
@@ -109,15 +100,11 @@ const Footer = () => {
                             <p className={styles.text}>
                                 <FaTwitter className={styles.icon} /> Twitter: @RecycleNow
                             </p>
-                            
-                            {/* New Contact Information Section */}
+
+                            {/* Contact Information Section */}
                             <p className={styles.head}>Contact Us</p>
-                            <p className={styles.text}>
-                                PO Box 2288-90100 Kisumu
-                            </p>
-                            <p className={styles.text}>
-                                 contact@RecycleNow.com
-                                 </p>
+                            <p className={styles.text}>PO Box 2288-90100 Kisumu</p>
+                            <p className={styles.text}>contact@RecycleNow.com</p>
                         </div>
                     </div>
                 </div>
@@ -125,14 +112,14 @@ const Footer = () => {
 
             {/* Embedded Map Section */}
             <div style={{ marginTop: '20px', textAlign: 'center' }}>
-                <iframe 
-                    src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3988.394398688508!2d37.275028174966195!3d-1.5307949984548723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMcKwMzEnNTAuOSJTIDM3wrAxNiczOS40IkU!5e0!3m2!1sen!2ske!4v1735552202036!5m2!1sen!2ske" 
-                    width="100%" 
-                    height="300" 
-                    style={{ border: 0 }} 
-                    title="Location Map of RecycleNow Ltd"
-                    allowFullScreen 
-                    loading="lazy" 
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127674.07694704065!2d34.738168599999995!3d-0.07497285000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182aa437ad4ac81d%3A0x2012a439d6248dd2!2sKisumu!5e0!3m2!1sen!2ske!4v1739730368087!5m2!1sen!2ske"
+                    width="100%"
+                    height="300"
+                    style={{ border: 0 }}
+                    title="Location Map of Kisumu"
+                    allowFullScreen
+                    loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
             </div>
