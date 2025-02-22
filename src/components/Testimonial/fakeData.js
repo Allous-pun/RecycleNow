d680d917774ec935a1454d3ef17cf59896b439a5
suleimanKenan961 <sulee9610@gmain.com>
2025-02-22 12:52:00 +0300

    Initial commits

--- src/components/Testimonial/fakeData.js
+++ src/components/Testimonial/fakeData.js
@@ -1,36 +1,41 @@
-
+import img1 from './images/d1.jpeg';
+import img2 from './images/d2.jpeg';
+import img3 from './images/d3.jpeg';
+import img4 from './images/d4.jpeg';
+import img5 from './images/d5.jpeg';
+import img6 from './images/d6.jpeg';
 export const fakeData = [
     {
         id: 1,
-        img: 'https://via.placeholder.com/150', 
+        img: img1, 
         name: 'Ian Dan',
         profession: 'Founder & CEO, RecycleNow',
         message: "Our innovative waste management methods have not only helped in reducing waste but also created new livelihood opportunities. By turning waste into valuable resources, we are promoting sustainable practices and contributing to a cleaner environment."
     },
     {
         id: 2,
-        img: 'https://via.placeholder.com/150', 
+        img: img2, 
         name: 'Nelson T',
         profession: 'NEMA DIRECTOR',
         message: "Managing municipal solid waste has been a challenge, but with the new waste management system, we have seen improvements in waste collection and disposal. The system has helped us address financial constraints and improve public awareness about sustainable waste management practices."
     },
     {
         id: 3,
-        img: 'https://via.placeholder.com/150',
+        img: img3,
         name: 'Lavendar Akinyi',
         profession: 'Tenant',
         message: "Since the implementation of RecycleNow in our Estate, we have seen a significant improvement in cleanliness and overall living conditions. The door-to-door waste collection service has made it easier for us to dispose of our waste responsibly, and the environment is much cleaner now."
     },
     {
         id: 4,
-        img: 'https://via.placeholder.com/150', 
+        img: img6, 
         name: 'Alan Wekesa',
         profession: 'Director, T & B Ltd',
         message: "Our innovative waste management methods, supported by RecycleNow, have not only helped in reducing waste but also created new livelihood opportunities. By turning waste into valuable resources, we are promoting sustainable practices and contributing to a cleaner environment."
     },
     {
         id: 5,
-        img: 'https://via.placeholder.com/150',
+        img: img5,
         name: 'Tony Wekesa',
         profession: 'Businessman',
         message:  "RecycleNow's platform has expanded our reach and boosted sales of eco-friendly products. We're proud to be part of a movement towards a sustainable future."
@@ -38,7 +43,7 @@ export const fakeData = [
     },
     {
         id: 6,
-        img: 'https://via.placeholder.com/150',
+        img: img4,
         name: 'Musa Mona',
         profession: 'Tech Entrepreneur',
         message: "RecycleNow has been a key partner in our journey to digital transformation. Their innovative solutions and commitment to excellence are unmatched."
