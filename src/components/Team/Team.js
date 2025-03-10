52fc78032f80927b8c2d3dbfcce50717fbdeefd6
pundit <pundit@pundit.pundit.com>
2025-03-10 12:34:55 +0300

    Refactored backend to Django, removed Node.js dependencies

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,18 +4,17 @@ import teamMember1 from '../../assets/home/melody.jpg';
 import teamMember2 from '../../assets/home/profile.jpeg'; 
 import teamMember3 from '../../assets/home/testimonial1.png'; 
 import teamMember4 from '../../assets/home/testimonial2.png'; 
-import teamMember5 from '../../assets/home/testimonial3.png'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const teamMembers = [
   {
     id: 1,
-    name: 'Melody Gichini',
+    name: 'Faith Chezolla ',
     role: 'CEO & Founder',
     image: teamMember1,
     expertise: 'Leadership, Business Strategy, and Product Development.',
-    bio: 'Melody has over 5 years of experience in the tech industry, leading teams and building innovative products.',
+    bio: 'Faith has over 5 years of experience in the tech industry, leading teams and building innovative products.',
     socialLinks: {
       twitter: 'https://twitter.com/melodygichini',
       linkedIn: 'https://www.linkedin.com/in/melody-gichini-202808250/',
@@ -24,11 +23,11 @@ const teamMembers = [
   },
   {
     id: 2,
-    name: 'Nelson Tommogo',
+    name: 'Molday Fidel',
     role: 'Engineering Manager',
     image: teamMember2,
     expertise: 'Mobile & Web Engineering, FullStack Development',
-    bio: 'Nelson is a seasoned Engineering Manager with extensive experience in overseeing development teams and implementing complex systems.',
+    bio: 'Fidel is a seasoned Engineering Manager with extensive experience in overseeing development teams and implementing complex systems.',
     socialLinks: {
       twitter: 'https://x.com/nelson_tommogo',
       linkedIn: 'https://www.linkedin.com/in/nelson-tommogo/',
@@ -37,11 +36,11 @@ const teamMembers = [
   },
   {
     id: 3,
-    name: 'Edins Gabina',
+    name: 'Suleiman Owiyo',
     role: 'Senior Software Engineer',
     image: teamMember3,
     expertise: 'Mobile Development',
-    bio: 'Edins specializes in mobile application development with over a decade of experience in creating user-friendly mobile solutions.',
+    bio: 'Suileiman specializes in mobile application development with over a decade of experience in creating user-friendly mobile solutions.',
     socialLinks: {
       twitter: 'https://twitter.com/edinsgabina',
       linkedIn: 'https://www.linkedin.com/in/edins-gabina-895925189/',
@@ -50,28 +49,15 @@ const teamMembers = [
   },
   {
     id: 4,
-    name: 'Sylvia Armony',
+    name: 'Aloyce Otieno',
     role: 'Chief Marketing Officer',
     image: teamMember4,
     expertise: 'Marketing Strategy, Brand Management, and Communications.',
-    bio: 'Ruth is a marketing expert with a proven track record of leading global campaigns and enhancing brand visibility.',
+    bio: 'Aloyce is a marketing expert with a proven track record of leading global campaigns and enhancing brand visibility.',
     socialLinks: {
       twitter: 'https://twitter.com/sylviaarmony',
       linkedIn: 'https://linkedin.com/in/sylviaarmony',
       github: 'https://github.com/sylviaarmony',
-    },
-  },
-  {
-    id: 5,
-    name: 'Tony Works',
-    role: 'FullStack Engineer',
-    image: teamMember5,
-    expertise: 'Web Development',
-    bio: 'Tony is a skilled FullStack Engineer with experience in building robust web applications and optimizing user experiences.',
-    socialLinks: {
-      twitter: 'https://twitter.com/tonyworks',
-      linkedIn: 'https://linkedin.com/in/tonyworks',
-      github: 'https://github.com/tonyworks',
     },
   },
 ];
