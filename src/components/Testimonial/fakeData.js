3c3f9c0c848618df9506c76a9c67caeb97c91eca
pundit <pundit@pundit.pundit.com>
2025-03-14 12:30:55 +0300

    testimonials

--- src/components/Testimonial/fakeData.js
+++ src/components/Testimonial/fakeData.js
@@ -1,33 +1,24 @@
-import manuImage from '../../assets/testimonials/manu.jpg';
-import melodyImage from '../../assets/testimonials/melody.jpg';
-import tonyImage from '../../assets/testimonials/Tony.jpg';
-import profile from '../../assets/testimonials/profile.jpeg';
-import testimonial from '../../assets/testimonials/example.png';
 export const fakeData = [
     {
-        id: 2,
-        img: manuImage, 
+        id: 2, 
         name: 'Nelson T',
         profession: 'NEMA DIRECTOR',
         message: "Managing municipal solid waste has been a challenge, but with the new waste management system, we have seen improvements in waste collection and disposal. The system has helped us address financial constraints and improve public awareness about sustainable waste management practices."
     },
     {
         id: 3,
-        img: melodyImage,
         name: 'Lavendar Akinyi',
         profession: 'Tenant',
         message: "Since the implementation of RecycleNow in our Estate, we have seen a significant improvement in cleanliness and overall living conditions. The door-to-door waste collection service has made it easier for us to dispose of our waste responsibly, and the environment is much cleaner now."
     },
     {
         id: 4,
-        img: tonyImage, 
         name: 'Alan Wekesa',
         profession: 'Director, T & B Ltd',
         message: "Our innovative waste management methods, supported by RecycleNow, have not only helped in reducing waste but also created new livelihood opportunities. By turning waste into valuable resources, we are promoting sustainable practices and contributing to a cleaner environment."
     },
     {
         id: 5,
-        img: profile,
         name: 'Tony Wekesa',
         profession: 'Businessman',
         message:  "RecycleNow's platform has expanded our reach and boosted sales of eco-friendly products. We're proud to be part of a movement towards a sustainable future."
@@ -35,7 +26,6 @@ export const fakeData = [
     },
     {
         id: 6,
-        img: testimonial,
         name: 'Musa Mona',
         profession: 'Tech Entrepreneur',
         message: "RecycleNow has been a key partner in our journey to digital transformation. Their innovative solutions and commitment to excellence are unmatched."
